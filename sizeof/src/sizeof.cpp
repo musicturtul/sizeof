c984ffe7a3624b11a1fa1efc7e70779b2fb2aee1
Kevin Hallman <musicturtul@192.168.1.103>
2018-06-04 19:58:42 -0400

    how to use sizeof to figure out how many rows and columns in array

--- sizeof/src/sizeof.cpp
+++ sizeof/src/sizeof.cpp
@@ -10,10 +10,40 @@
 using namespace std;
 
 int main() {
-	int newArray[] = {4, 2, 6, 9};
+	int newArray[] = { 4, 2, 6, 9, 11, 21, 25, 444, 156 };
 
-	for (int i = 0; i < 4; i++) {
+	cout << sizeof(newArray) << endl;
+	cout << sizeof(int) << endl;
+
+	for (unsigned int i = 0; i < sizeof(newArray) / sizeof(int); i++) {
 		cout << newArray[i] << " " << flush;
+	}
+	cout << endl;
+
+	unsigned int nextArray[6][12];
+	int numberRows = sizeof(nextArray) / sizeof(nextArray[0]);
+	int numberColumns = sizeof(nextArray[0]) / sizeof(unsigned int);
+
+   for (unsigned int i = 0; i < numberRows; i++){
+	   for(unsigned int j = 0; j < numberColumns; j++){
+		   nextArray[i][j] = (i + 1) * (j + 1);
+	   }
+   }
+
+   for (unsigned int i = 0; i < sizeof(nextArray) / sizeof(nextArray[0]); i++) {
+	   for (unsigned int j = 0; j < sizeof(nextArray[0]) / sizeof(unsigned int); j++) {
+		   cout << nextArray[i][j] << " "  << flush;
+	   }
+	   cout << endl;
+   }
+
+	int MULTI_ARRAY[3][2] { { 1, 5 }, { 6, 8 }, { 4, 2 } };
+
+	for (int i = 0; (i < sizeof(MULTI_ARRAY) / sizeof(int) / 2); i++) {
+		for (int j = 0; (j < sizeof(MULTI_ARRAY) / sizeof(int) / 3); j++) {
+			std::cout << MULTI_ARRAY[i][j] << " " << std::flush;
+		}
+		std::cout << std::endl;
 	}
 
 	return 0;
